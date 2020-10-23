cdf9a230a8b520d520bcac782c8957e6eaf162de
cdunarea <cdunarea@cisco.com>
2020-10-22 21:52:06 -0600

    add/fix mocks

---
+++ PackageManager/PackageManagerClient/tests/mocks/utilMocks/MockPmHttp.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "MocksCommon.h"
+
+#include "IPmHttp.h"
+
+class MockPmHttp : public IPmHttp
+{
+public:
+    MockPmHttp();
+    ~MockPmHttp();
+
+    MOCK_METHOD3( Init, int32_t( PM_PROGRESS_CALLBACK, void*, std::string& ) );
+    void MakeInitReturn( int32_t value );
+    void ExpectInitIsNotCalled();
+
+    MOCK_METHOD0( Deinit, int32_t() );
+    void MakeDeinitReturn( int32_t value );
+    void ExpectDeinitIsNotCalled();
+
+    MOCK_METHOD1( SetToken, int32_t( std::string& ) );
+    void MakeSetTokenReturn( int32_t value );
+    void ExpectSetTokenIsNotCalled();
+
+    MOCK_METHOD1( SetCerts, int32_t( const PmHttpCertList& ) );
+    void MakeSetCertsReturn( int32_t value );
+    void ExpectSetCertsIsNotCalled();
+
+    MOCK_METHOD3( HttpGet, int32_t( std::string&, std::string&, int32_t& ) );
+    void MakeHttpGetReturn( int32_t value );
+    void ExpectHttpGetIsNotCalled();
+
+    MOCK_METHOD5( HttpPost, int32_t( std::string&, void*, size_t, std::string&, int32_t& ) );
+    void MakeHttpPostReturn( int32_t value );
+    void ExpectHttpPostIsNotCalled();
+
+    MOCK_METHOD3( HttpDownload, int32_t( std::string&, std::string&, int32_t& ) );
+    void MakeHttpDownloadReturn( int32_t value );
+    void ExpectHttpDownloadIsNotCalled();
+
+};
