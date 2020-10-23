447fc6df3b61547e10c14eb9d7bf58bd1723b428
cdunarea <cdunarea@cisco.com>
2020-10-23 00:28:06 -0600

    ComponentPackageProcessor test and mock fixes

--- PackageManager/PackageManagerClient/tests/mocks/pmLibMocks/MockPmCloud.h
+++ PackageManager/PackageManagerClient/tests/mocks/pmLibMocks/MockPmCloud.h
@@ -10,24 +10,24 @@ public:
     MockPmCloud();
     ~MockPmCloud();
 
-    MOCK_METHOD1( SetUri, void( std::string& ) );
+    MOCK_METHOD1( SetUri, void( const std::string& ) );
     void ExpectSetUriIsNotCalled();
 
-    MOCK_METHOD1( SetToken, void( std::string& ) );
+    MOCK_METHOD1( SetToken, void( const std::string& ) );
     void ExpectSetTokenIsNotCalled();
 
     MOCK_METHOD1( SetCerts, void( const PmHttpCertList& ) );
     void ExpectSetCertsIsNotCalled();
 
-    MOCK_METHOD2( Checkin, int32_t( std::string&, std::string& ) );
+    MOCK_METHOD2( Checkin, int32_t( const std::string&, std::string& ) );
     void MakeCheckinReturn( int32_t value );
     void ExpectCheckinIsNotCalled();
 
-    MOCK_METHOD1( SendEvent, int32_t( std::string& ) );
+    MOCK_METHOD1( SendEvent, int32_t( const std::string& ) );
     void MakeSendEventReturn( int32_t value );
     void ExpectSendEventIsNotCalled();
 
-    MOCK_METHOD2( DownloadFile, int32_t( std::string&, std::string ) );
+    MOCK_METHOD2( DownloadFile, int32_t( const std::string&, const std::string ) );
     void MakeDownloadFileReturn( int32_t value );
     void ExpectDownloadFileIsNotCalled();
 
