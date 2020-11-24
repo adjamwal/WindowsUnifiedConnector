0bc6658eaf1c8265f07b12e9c8225a43b9cdea70
cdunarea <cdunarea@cisco.com>
2020-11-23 19:59:44 -0700

    changed to unit tests

--- SharedLibs/TestCloudEvents/TestCloudEventBuilder.cpp
+++ SharedLibs/TestCloudEvents/TestCloudEventBuilder.cpp
@@ -1,21 +1,13 @@
 #include "pch.h"
 
 #include "CloudEventBuilder.h"
-#include "MockHttpAdapter.h"
-#include "CloudEventPublisher.h"
-#include "IUcLogger.h"
-#include "EnumStringConversion.h"
-#include "json\json.h"
 #include <memory>
 
-class ComponentTestCloudEvents : public ::testing::Test
+class TestCloudEventBuilder : public ::testing::Test
 {
 protected:
     void SetUp()
     {
-        m_httpAdapter.reset( new NiceMock<MockHttpAdapter>() );
-        m_eventPublisher.reset( new CloudEventPublisher( *m_httpAdapter, "https://4m2294wzqf.execute-api.us-west-1.amazonaws.com/dev/identify/event" ) );
-
         m_eventBuilder
             .WithUCID( "5B3861FF-2690-45D4-A49D-8F8CD18BBFC6" )
             .WithType( CloudEventType::pkgreconfig )
@@ -28,60 +20,49 @@ protected:
     void TearDown()
     {
         m_eventBuilder.Reset();
-        m_httpAdapter.reset();
-        m_eventPublisher.reset();
     }
 
     CloudEventBuilder m_eventBuilder;
-    std::unique_ptr<MockHttpAdapter> m_httpAdapter;
-    std::unique_ptr<ICloudEventPublisher> m_eventPublisher;
 };
 
-TEST_F( ComponentTestCloudEvents, EventJSONContainsUCID )
+TEST_F( TestCloudEventBuilder, EventJSONContainsUCID )
 {
     std::string eventJson = m_eventBuilder.Build();
 
     ASSERT_TRUE( eventJson.find( "\"ucid\":\"5B3861FF-2690-45D4-A49D-8F8CD18BBFC6\"" ) != std::string::npos );
 }
 
-TEST_F( ComponentTestCloudEvents, EventJSONContainsEventType )
+TEST_F( TestCloudEventBuilder, EventJSONContainsEventType )
 {
     std::string eventJson = m_eventBuilder.Build();
 
     ASSERT_TRUE( eventJson.find( "\"type\":\"pkg-reconfig\"" ) != std::string::npos );
 }
 
-TEST_F( ComponentTestCloudEvents, EventJSONContainsPackage )
+TEST_F( TestCloudEventBuilder, EventJSONContainsPackage )
 {
     std::string eventJson = m_eventBuilder.Build();
 
     ASSERT_TRUE( eventJson.find( "\"package\":\"amp/1.0.0\"" ) != std::string::npos );
 }
 
-TEST_F( ComponentTestCloudEvents, EventJSONContainsOldFile )
+TEST_F( TestCloudEventBuilder, EventJSONContainsOldFile )
 {
     std::string eventJson = m_eventBuilder.Build();
 
     ASSERT_TRUE( eventJson.find( "\"old\":[{\"path\":\"oldfile\",\"sha256\":\"oldhash123\",\"size\":123}]" ) != std::string::npos );
 }
 
-TEST_F( ComponentTestCloudEvents, EventJSONContainsNewFile )
+TEST_F( TestCloudEventBuilder, EventJSONContainsNewFile )
 {
     std::string eventJson = m_eventBuilder.Build();
 
     ASSERT_TRUE( eventJson.find( "\"new\":[{\"path\":\"newfile\",\"sha256\":\"newhash123\",\"size\":234}]" ) != std::string::npos );
 }
 
-TEST_F( ComponentTestCloudEvents, EventJSONContainsError )
+TEST_F( TestCloudEventBuilder, EventJSONContainsError )
 {
     std::string eventJson = m_eventBuilder.Build();
 
     ASSERT_TRUE( eventJson.find( "\"err\":{\"code\":100,\"msg\":\"some error\"}" ) != std::string::npos );
-}
-
-TEST_F( ComponentTestCloudEvents, EventPublisherCallsHttpPost )
-{
-    EXPECT_CALL( *m_httpAdapter, HttpPost( _, _, _, _, _ ) ).Times( 1 );
-
-    m_eventPublisher->Publish( m_eventBuilder );
 }
---
+++ SharedLibs/TestCloudEvents/TestCloudEventPublisher.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+
+#include "CloudEventBuilder.h"
+#include "MockHttpAdapter.h"
+#include "CloudEventPublisher.h"
+#include <memory>
+
+class TestCloudEventPublisher : public ::testing::Test
+{
+protected:
+    void SetUp()
+    {
+        m_httpAdapter.reset( new NiceMock<MockHttpAdapter>() );
+        m_eventPublisher.reset( new CloudEventPublisher( *m_httpAdapter, "https://4m2294wzqf.execute-api.us-west-1.amazonaws.com/dev/identify/event" ) );
+
+        m_eventBuilder
+            .WithUCID( "5B3861FF-2690-45D4-A49D-8F8CD18BBFC6" )
+            .WithType( CloudEventType::pkgreconfig )
+            .WithPackage( "amp", "1.0.0" )
+            .WithOldFile( "oldfile", "oldhash123", 123 )
+            .WithNewFile( "newfile", "newhash123", 234 )
+            .WithError( 100, "some error" );
+    }
+
+    void TearDown()
+    {
+        m_eventBuilder.Reset();
+        m_httpAdapter.reset();
+        m_eventPublisher.reset();
+    }
+
+    CloudEventBuilder m_eventBuilder;
+    std::unique_ptr<MockHttpAdapter> m_httpAdapter;
+    std::unique_ptr<ICloudEventPublisher> m_eventPublisher;
+};
+
+TEST_F( TestCloudEventPublisher, EventPublisherCallsHttpPost )
+{
+    m_httpAdapter->MakeHttpPostReturn( 201 );
+    EXPECT_CALL( *m_httpAdapter, HttpPost( _, _, _, _, _ ) ).Times( 1 );
+    EXPECT_EQ( 201, m_eventPublisher->Publish( m_eventBuilder ) );
+}
