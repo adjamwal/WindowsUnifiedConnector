c8e73693fd9a4fed8bd4a8d7292bafb5346e344d
Raymond Dang <raydang@cisco.com>
2020-12-02 16:05:44 -0700

    AMP4E-12451: Only refresh token on 401 errors

--- PackageManager/libPmClient/CheckinManifestRetriever.cpp
+++ PackageManager/libPmClient/CheckinManifestRetriever.cpp
@@ -17,39 +17,46 @@ std::string CheckinManifestRetriever::GetCheckinManifestFrom( std::string uri, s
 {
     std::lock_guard<std::mutex> lock( m_mutex );
     std::string response;
-    bool checkinFailed = false;
+    int32_t respStatus;
 
-    try {
-        response = InternalGetCheckinManifestFrom( uri, payload );
-    }
-    catch ( std::exception& ex ) {
-        checkinFailed = true;
-    }
+    respStatus = InternalGetCheckinManifestFrom( uri, payload, response );
+    
+    if ( respStatus != 200 ) {
+        if ( respStatus != 401 ) {
+            HandleHttpError( respStatus );
+        }
+        else {
+            m_ucidAdapter.Refresh();
+            respStatus = InternalGetCheckinManifestFrom( uri, payload, response );
 
-    if ( checkinFailed ) {
-        m_ucidAdapter.Refresh();
-        response = InternalGetCheckinManifestFrom( uri, payload );
+            if ( respStatus != 200 ) {
+                HandleHttpError( respStatus );
+            }
+        }
     }
 
     return response;
 }
 
-std::string CheckinManifestRetriever::InternalGetCheckinManifestFrom( std::string uri, std::string payload )
+int32_t CheckinManifestRetriever::InternalGetCheckinManifestFrom( std::string& uri, std::string& payload, std::string& response )
 {
+    std::string token = m_ucidAdapter.GetAccessToken();
+    if ( token.empty() ) {
+        return 0;
+    }
+
     m_cloud.SetUri( uri );
-    m_cloud.SetToken( m_ucidAdapter.GetAccessToken() );
+    m_cloud.SetToken( token );
     m_cloud.SetCerts( m_certsAdapter.GetCertsList() );
 
     LOG_DEBUG( "Checkin uri:%s, payload:%s", uri.c_str(), payload.c_str() );
 
-    std::string response;
-    int32_t respStatus = m_cloud.Checkin( payload, response );
+    return m_cloud.Checkin( payload, response );
+}
 
-    if ( respStatus != 200 ) {
-        std::string s = __FUNCTION__ ": Http Post status ";
-        s += std::to_string( respStatus );
-        throw std::exception( s.c_str() );
-    }
-
-    return response;
+void CheckinManifestRetriever::HandleHttpError( int32_t respStatus )
+{
+    std::string s = __FUNCTION__ ": Http Post status ";
+    s += std::to_string( respStatus );
+    throw std::exception( s.c_str() );
 }
