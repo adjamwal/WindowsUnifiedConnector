fa95891938c11d33fa388be7775fcfd572bb5f50
cdunarea <cdunarea@cisco.com>
2020-12-10 23:26:51 -0700

    optimization and cleanup

--- PackageManager/libPmUtil/RandomUtil.cpp
+++ PackageManager/libPmUtil/RandomUtil.cpp
@@ -1,56 +1,59 @@
 #include "RandomUtil.h"
-#include <numeric>
+#include <random>
 #include <algorithm>
 #include <chrono>
+#include <numeric>
+#include <atomic>
 
 namespace
 {
     const std::string availableChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     const std::wstring availableWChars = L"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static std::mt19937 generator( std::random_device {}( ) );
+    static std::uniform_int_distribution<int> distribution { std::numeric_limits<int>::lowest(), std::numeric_limits<int>::max() };
+    static std::atomic_int sequence = distribution( generator );
 }
 
 std::vector<uint8_t> RandomUtil::Bytes( std::size_t count )
 {
-    std::vector<uint8_t> data( count * ( 5 + GetNanos() % 6 ) );
+    std::vector<uint8_t> data( count * ( 2 + ( sequence + GetNanos() ) % 6 ) );
     std::iota( data.begin(), data.end(), 0 );
-    std::shuffle( data.begin(), data.end(), std::mt19937 { std::random_device { }( ) } );
+    std::shuffle( data.begin(), data.end(), generator );
     data.resize( count );
     return data;
 }
 
 std::vector<uint16_t> RandomUtil::Words( std::size_t count )
 {
-    std::vector<uint16_t> data( count * ( 5 + GetNanos() % 6 ) );
+    std::vector<uint16_t> data( count * ( 2 + ( sequence + GetNanos() ) % 6 ) );
     std::iota( data.begin(), data.end(), 0 );
-    std::shuffle( data.begin(), data.end(), std::mt19937 { std::random_device { }( ) } );
+    std::shuffle( data.begin(), data.end(), generator );
     data.resize( count );
     return data;
 }
 
 std::vector<uint32_t> RandomUtil::DWords( std::size_t count )
 {
-    std::vector<uint32_t> data( count * ( 5 + GetNanos() % 6 ) );
+    std::vector<uint32_t> data( count * ( 2 + ( sequence + GetNanos() ) % 6 ) );
     std::iota( data.begin(), data.end(), 0 );
-    std::shuffle( data.begin(), data.end(), std::mt19937 { std::random_device { }( ) } );
+    std::shuffle( data.begin(), data.end(), generator );
     data.resize( count );
     return data;
 }
 
 std::vector<uint64_t> RandomUtil::QWords( std::size_t count )
 {
-    std::vector<uint64_t> data( count * ( 5 + GetNanos() % 6 ) );
+    std::vector<uint64_t> data( count * ( 2 + ( sequence + GetNanos() ) % 6 ) );
     std::iota( data.begin(), data.end(), 0 );
-    std::shuffle( data.begin(), data.end(), std::mt19937 { std::random_device { }( ) } );
+    std::shuffle( data.begin(), data.end(), generator );
     data.resize( count );
     return data;
 }
 
 int RandomUtil::GetInt( int min, int max )
 {
-    std::mt19937 randomNumberGenerator = std::mt19937( GetSeed() );
-    std::uniform_int_distribution<int> uni( min, max );
-
-    return uni( randomNumberGenerator );
+    std::uniform_int_distribution<int> range( min, max );
+    return range( generator );
 }
 
 std::string RandomUtil::GetString( int minLength, int maxLength )
@@ -81,8 +84,8 @@ std::wstring RandomUtil::GetWString( int minLength, int maxLength )
 
 uint64_t RandomUtil::GetSeed()
 {
-    unsigned int seed = GetNanos();
-    auto seedData = DWords( 5 + seed % 6 );
+    unsigned int seed = sequence + GetNanos();
+    auto seedData = DWords( 1 + seed % 6 );
     for( auto element : seedData ) {
         seed += element;
     }
@@ -92,8 +95,9 @@ uint64_t RandomUtil::GetSeed()
 
 uint64_t RandomUtil::GetNanos()
 {
-    std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
-    std::chrono::system_clock::duration nanos = tp.time_since_epoch();
+    auto origin = std::chrono::system_clock::from_time_t( time_t( -12219292800 ) );
+    auto tspan = std::chrono::system_clock::now() - origin;
+    auto nanos = std::chrono::duration_cast< std::chrono::nanoseconds >( tspan ).count();
 
-    return nanos.count();
+    return nanos;
 }
