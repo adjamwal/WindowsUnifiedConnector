898952da1fdbb93ff579c9e73108cc9e43f1f9bc
Raymond Dang <raydang@cisco.com>
2021-01-11 10:46:30 -0700

    Missed the actual service name

--- UC/UCService/stdafx.h
+++ UC/UCService/stdafx.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#define SERVICE_NAME             L"UCService" 
-#define SERVICE_DISPLAY_NAME     L"Windows Unified Connector Service" 
+#if UC_CONSUMER == 1
+#define SERVICE_NAME             L"ImmunetUnifiedConnector"
+#define SERVICE_DISPLAY_NAME     L"Immunet Unified Connector"
+#else
+#define SERVICE_NAME             L"CiscoUnifiedConnector"
+#define SERVICE_DISPLAY_NAME     L"Cisco Unified Connector"
+#endif
 #define SERVICE_START_TYPE       SERVICE_DEMAND_START 
 #define SERVICE_DEPENDENCIES     L"" 
 #define SERVICE_ACCOUNT          L"NT AUTHORITY\\System" 
