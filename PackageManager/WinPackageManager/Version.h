1852cafdd7e7d1c79ee9e51ee1a6bfe5e0b17a8a
kdeviat <kdeviat@cisco.com>
2021-01-22 00:29:02 -0700

    Update executable names to ucs.exe and ucpm.exe

--- PackageManager/WinPackageManager/Version.h
+++ PackageManager/WinPackageManager/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define LOCALSTRFILEDESC "Cisco Unified Connector Package Manager\0"
-#define LOCALSTRFILENAME "UC_PM.exe\0"
-#define LOCALSTRINTERNALFILENAME "UC_PM.exe\0"
+#define LOCALSTRFILENAME "ucpm.exe\0"
+#define LOCALSTRINTERNALFILENAME "ucpm.exe\0"
 #include "..\..\GlobalVersion.h"
--- UC/UCService/Version.h
+++ UC/UCService/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define LOCALSTRFILEDESC "Cisco Unified Connector Service\0"
-#define LOCALSTRFILENAME "UCService.exe\0"
-#define LOCALSTRINTERNALFILENAME "UCService.exe\0"
+#define LOCALSTRFILENAME "ucs.exe\0"
+#define LOCALSTRINTERNALFILENAME "ucs.exe\0"
 #include "..\..\GlobalVersion.h"
