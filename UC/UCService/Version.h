ccc8bd04a1da9d17593ec546a62ace254cb1a46b
raydang <raydang@cisco.com>
2021-05-28 15:45:30 -0600

    AMP4E-14264 Update branding folders/filenames

--- UC/UCService/Version.h
+++ UC/UCService/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define LOCALSTRFILEDESC "Cisco Unified Connector Service\0"
-#define LOCALSTRFILENAME "ucs.exe\0"
-#define LOCALSTRINTERNALFILENAME "ucs.exe\0"
+#define LOCALSTRFILENAME "csc_uc.exe\0"
+#define LOCALSTRINTERNALFILENAME "csc_uc.exe\0"
 #include "..\..\GlobalVersion.h"
