9b577f0b41ff367c4eee8227f036edbc3be22115
cdunarea <cdunarea@cisco.com>
2021-06-01 12:00:06 -0600

    prevent uninstall failure on ucid errors

--- Install/InstallerCustomActions/CustomAction.cpp
+++ Install/InstallerCustomActions/CustomAction.cpp
@@ -161,9 +161,9 @@ UINT __stdcall StoreUCIDToProperty( MSIHANDLE hInstall )
     }
 
 LExit:
-    er = SUCCEEDED( hr ) ? ERROR_SUCCESS : ERROR_INSTALL_FAILURE;
     SetUcLogger( NULL );
-    return WcaFinalize( er );
+    //always ignore UCID errors
+    return WcaFinalize( ERROR_SUCCESS );
 }
 
 UINT __stdcall SendEventOnUninstallBegin( MSIHANDLE hInstall )
